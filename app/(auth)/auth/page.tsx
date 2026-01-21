69c6f8a3690a7a4c258760aedbeba71730ffc266
Axl Santos <axlsntz.dev@gmail.com>
2026-01-20 19:08:38 -0600

    feat: update login page layout and enhance image styling

--- app/(auth)/auth/page.tsx
+++ app/(auth)/auth/page.tsx
@@ -3,7 +3,7 @@ import {Suspense} from "react";
 
 export default function LoginPage() {
     return (
-            <div className="grid min-h-svh lg:grid-cols-2">
+            <div className="grid min-h-svh lg:grid-cols-2 ">
                 <div className="flex flex-col gap-4 p-6 md:p-10">
                     <div className="flex  gap-2 justify-start">
                         <img src={"/images/lumen-ems.png"} alt={"logo"} className={"w-1/2 md:w-1/3"}/>
@@ -14,11 +14,11 @@ export default function LoginPage() {
                         </div>
                     </div>
                 </div>
-                <div className="bg-muted relative hidden lg:block">
+                <div className="bg-muted relative hidden lg:block m-6 rounded-4xl">
                     <img
-                        src="/images/auth-image-2.png"
+                        src="/images/auth-page-image.jpg"
                         alt="Image"
-                        className="absolute inset-0 h-full w-full object-cover"
+                        className="absolute inset-0 h-full w-full object-cover rounded-4xl shadow-xs"
                     />
                 </div>
             </div>
